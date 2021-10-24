78153dc855d5b6be6658bd4a8c96bd3840d5f365
Érico Sabino <ericosabino@Ericos-MacBook-Pro.local>
2021-10-23 21:04:12 -0400

    final challenge files

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# surfs_up
+# Surf's Up
+
+## Overview of the Analysis
+
+This analysis consists of gathering the temperature data for the months of June and December in Oahu, in order to determine if the surf and ice cream shop business is sustainable year-round.
+
+With that in mind, two queries were used on the given data base to find the results.Queries:
+
+`june_temps = session.query(Measurement.date, Measurement.tobs).filter(extract('month', Measurement.date)==6).all()`
+
+`december_temps = session.query(Measurement.date, Measurement.tobs).filter(extract('month', Measurement.date)==12).all()`
+
+## Results
+
+By querying the data, transforming the results in data frames and describing the statistical data for the temperature, the results found are below:
+
+June Temperatures | December Temperatures
+:-------------------------:|:-------------------------:
+![](/Resources/June_Temps.png)  |  ![](/Resources/December_Temps.png)
+
+Some results are listed below:
+* The average temp in June is 75F while in December it is 71F, which means it's not very cold in December and business can be ran at that time, on average.
+* The min temp is 64F in June and 56F in December, which indicates a potential drop in ice cream sales and surfers frequency in the colder days.
+* The max temp is 85F in June and 83F in December, which indicates a great weather for both surfing and ince cream sales in both months.
+
+
+## Summary
+
+Based only on the weather data, it looks that both June and December are good months to make business in Surf and Ice Cream in Oahu, with an execption for the cold days in December. In order to expand this analysis, additional data could be gathered:
+
+1. Wind: wind data could determine how the wind affects the variance of the temperature for these months.
+
+2. Precipitation: this would give more insights on the number of cloudy days in these months, which could also influence sales.
+
+Besides weather information, I would also collect tourism data to understand what are the periods of the year which there is an increase in the number of tourists, or even the number of bathers on the beach.
--- app.py
+++ app.py
@@ -1,10 +1,90 @@
-from flask import Flask
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
+# Access data base
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# Reflect the data base
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+#Saving references to the database
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Creating a session
+session = Session(engine)
+
+# Setting up Flask
 app = Flask(__name__)
+
 
 
 # Also knows as the route
 @app.route('/')
 
-def hello_world():
-    return 'Hello world'
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!\n
+    Available Routes:\n
+    /api/v1.0/precipitation\n
+    /api/v1.0/stations\n
+    /api/v1.0/tobs\n
+    /api/v1.0/temp/start/end\n
+    ''')
+
+@app.route("/api/v1.0/precipitation")
+
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
+
+
+@app.route("/api/v1.0/stations")
+
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
+
+@app.route("/api/v1.0/tobs")
+
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+      filter(Measurement.station == 'USC00519281').\
+      filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+
+
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+
+
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps=temps)
+
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps)
